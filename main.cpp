2d160c204128efccbfa077931a26d7e235b36b50
Wangs <1297257207@qq.com>
2025-05-05 20:13:50 +0800

    new666

--- main.cpp
+++ main.cpp
@@ -1,10 +1,15 @@
 #include <bits/stdc++.h>
 #include "mytest.cpp"
+#include "New666Test.cpp"
 using namespace std;
 
 int main() {
     cout << "project1111" << endl;
     cout << "hahaha" <<endl;
     cout_a();
+
+    int x;
+    cin>>x;
+    new666(x);
     return 0;
 }
