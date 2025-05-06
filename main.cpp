b5224eb0b06d95c86505ef8275a0d5ae2d903816
Wangs <1297257207@qq.com>
2025-05-06 21:20:21 +0800

    [T22] 燃尽图开发

--- main.cpp
+++ main.cpp
@@ -1,14 +1,15 @@
 #include <bits/stdc++.h>
-#include "BornDownChart.cpp"
-using namespace std;
+
+#include "BornChart.cpp"
 
 int main() {
+
     int totalTasks = 100;
     int totalDays = 20;
     bool randomBurn = true;
-
     std::vector<int> burndown = simulateBurndown(totalTasks, totalDays, randomBurn);
     printBurndownChart(burndown);
+
 
     return 0;
 }
